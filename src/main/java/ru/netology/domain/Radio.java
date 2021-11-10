260346d0c5b932068d932b2dcc9366b8f0f3a916
Yakushin Alexandr <valensiec08@mail.ru>
2021-11-10 21:29:49 +0300

    edit Radio.java

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+public class Radio {
+    int currentStation;
+    int currentVolume;
+
+    public void increaseStation() {
+        if (currentStation <= 9) {
+            currentStation = currentStation + 1;
+        }
+        if (currentStation == 10) {
+            currentStation = 0;
+        }
+    }
+
+    public void decreaseStation() {
+        if (currentStation >= 0) {
+            currentStation = currentStation - 1;}
+
+                if (currentStation == -1) {
+                    currentStation = 9;
+                }
+            }
+        }
+    }
